2a1154324b1872dbc8682e71fcccb82967e50613
kristinetuazon <kristine.jane.tuazon@gmail.com>
2022-10-30 09:41:45 +0900

    feat: adds top/bottom text input box and generator button

---
+++ client/src/components/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingScreen: React.FC = () => {
+    return (
+        <h1>Start Screen</h1>
+    )
+}
+
+export default LoadingScreen;
--- client/src/components/MemeGenerator.tsx
+++ client/src/components/MemeGenerator.tsx
@@ -1,26 +1,28 @@
 import { response } from "express";
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import LoadingScreen from './LoadingScreen';
+
+interface Image {
+  box_count: number,
+  height: number,
+  id: string,
+  name: string,
+  url: string,
+  width: number,
+}; 
 
 const MemeGenerator: React.FC = () => {
-const [upText, setUpText] = useState <string | null> (null);
-const [bottomText, setButtonText] = useState < string | null> (null);
+const [topText, setTopText] = useState <string> ("");
+const [bottomText, setBottomText] = useState < string> ("");
 const [images, setImages] = useState < []| null> (null);
 
 useEffect (() => {
     handleSetImages()
-  }, []);
+  }, [<LoadingScreen />]);
 
-  interface Image {
-    box_count: number,
-    height: number,
-    id: string,
-    name: string,
-    url: string,
-    width: number,
-}; 
 
-  const handleSetImages = async (): Promise<any> => {
+const handleSetImages = async (): Promise<any> => {
     const response = await axios('/photos');
     const responseObject = response.data.memes
     const arrImgUrl = responseObject.map((object: Image, index: number) => {
@@ -36,10 +38,30 @@ useEffect (() => {
     setImages(arrImgUrl);
 };
 
+const onTextChange = (event: any) => {
+    setBottomText(event.target.value)
+    setTopText(event.target.value)
+}
+
     return (
+        <>
+        <form action="" className="memeGallery__form">
+            <input name="topText" 
+            type="text" 
+            value={topText}
+            onChange = {onTextChange}
+            />
+             <input name="bottomText" 
+            type="text" 
+            value={bottomText}
+            onChange = {onTextChange}
+            />
+            <button>Make Me Laugh</button>
+        </form>
         <div className="memeGallery">
            {images}
         </div>
+        </>
     )
 }
 
